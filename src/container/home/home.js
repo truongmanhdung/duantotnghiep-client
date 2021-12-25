b279329f0d218faa7c1701e27250b7289cef4c64
DangTrongTruong <84057666+DangTrongTruong@users.noreply.github.com>
2021-12-25 22:44:08 +0700

    Update home.js

--- src/container/home/home.js
+++ src/container/home/home.js
@@ -10,7 +10,7 @@ function Home(props) {
     }
     return (
         <div>
-            <p>Đây là home page</p>
+            <p>Bo may la Hai</p>
             <button onClick={handleClick}>click</button>
         </div>
     )
